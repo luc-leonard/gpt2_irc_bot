5f0ddefe8f001cad0410b47473f602694f82d63b
Luc Léonard <github@luc-leonard.fr>
2021-02-10 22:08:12 +0100

    initial commit

---
+++ irc_bot.py
@@ -0,0 +1,103 @@
+import argparse
+import textwrap
+
+import irc
+import irc.bot
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from transformers import pipeline
+import random
+
+# model = GPT2LMHeadModel.from_pretrained("./anarj_clean_bot/")
+# tokenizer = GPT2Tokenizer.from_pretrained("antoiloui/belgpt2")
+#
+# generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+#
+# result = generator('J\'aime ',
+#                    max_length=300,
+#                    do_sample=False,
+#                    top_p=0.95,
+#                    temperature=1,
+#                    top_k=500,
+#                    num_beams=4,
+#                    no_repeat_ngram_size=2,
+#                    num_return_sequences=1)
+#
+# print(result)
+
+
+class IrcBot(irc.bot.SingleServerIRCBot):
+
+    def __init__(self, channel: str, nickname: str, server: str, generator, wakeword,
+                 port=6667):
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
+
+        self.channel = channel
+        self._generator = generator
+        self._wakeword = wakeword
+
+    def on_nicknameinuse(self, c: irc.client, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c: irc.client.ServerConnection, e: irc.client.Event):
+        ...
+
+    def on_pubmsg(self, c, e):
+        text = e.arguments[0]
+        if len(text) > 100:
+            text = text[:99]
+        args = text.split()
+        if args[0] == '!' + self._wakeword:
+            text = self._generator(' '.join(args[1:]),
+                                   max_length=100,
+                                   device=0,
+                                   do_sample=False,
+                                   top_p=random.randint(50, 100) / 100,
+                                   temperature=1,
+                                   top_k=500,
+                                   num_beams=4,
+                                   no_repeat_ngram_size=2,
+                                   num_return_sequences=1)[0]['generated_text']
+            wrapped = textwrap.fill(text, 400)
+            for line in wrapped.splitlines(keepends=False):
+                c.privmsg(e.target, line)
+
+
+    def on_dccmsg(self, c, e):
+        pass
+
+    def on_dccchat(self, c, e):
+        pass
+
+    def do_command(self, e, cmd):
+        pass
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="the model")
+    parser.add_argument("--name", help="the name")
+    parser.add_argument("--wakeword",
+                        help="wakeword")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    print("loading generators...")
+    model = GPT2LMHeadModel.from_pretrained(args.model).to('cuda')
+    tokenizer = GPT2Tokenizer.from_pretrained("antoiloui/belgpt2")
+    print('generator loaded...')
+    generator = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0)
+
+    bot = IrcBot("#reddit-fr", args.name, "irc.redditairfrance.fr",
+                 generator, args.wakeword)
+
+    bot.start()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
---
+++ train_bot.py
@@ -0,0 +1,68 @@
+import argparse
+from typing import Optional
+
+from numpy.core.setup_common import OPTIONAL_STDFUNCS_MAYBE
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, LineByLineTextDataset
+from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
+
+
+class Config:
+    output_dir: str
+    input_model: Optional[str]
+    input_file: str
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_dir", help="Where to store the model ?")
+    parser.add_argument("--input_file", help="The input file. Each record must be on its line")
+    parser.add_argument("--input_model",
+                        help="the folder of a previously trained model. If you want to continue training it", default=None)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    if args.input_model is None:
+        model = GPT2LMHeadModel.from_pretrained("antoiloui/belgpt2")
+    else:
+        print('loading pre trained model')
+        model = GPT2LMHeadModel.from_pretrained(args.input_model)
+
+    tokenizer = GPT2Tokenizer.from_pretrained("antoiloui/belgpt2")
+
+    training_args = TrainingArguments(
+        output_dir=args.output_dir + '_checkpoint',  # output directory
+        num_train_epochs=3,  # total number of training epochs
+        per_device_train_batch_size=64,  # batch size per device during training
+        warmup_steps=100,  # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,  # strength of weight decay
+        logging_dir='./logs_hyca',  # directory for storing logs
+        logging_steps=100,
+    )
+    special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>', 'pad_token': '<PAD>'}
+    tokenizer.add_special_tokens(special_tokens_dict)
+
+    model.resize_token_embeddings(len(tokenizer))
+    dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path=args.input_file, block_size=32)
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=False
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=dataset,
+    )
+    if args.input_model is not None:
+        trainer.train(resume_from_checkpoint=args.input_model + '_checkpoint')
+    else:
+        trainer.train()
+    model.save_pretrained(args.output_dir)
+
+
+if __name__ == '__main__':
+    main()
